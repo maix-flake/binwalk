49f38045fe7a8b4256d7a854600feb7885b49fb2
maix0 <maieul.dev@familleboyer.net>
2024-10-18 17:27:49 +0200

    update: first pass

---
+++ flake.nix
@@ -0,0 +1,35 @@
+{
+  inputs = {
+    flake-utils.url = "github:numtide/flake-utils";
+    naersk.url = "github:nix-community/naersk";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+    binwalk-git = {
+      url = "github:ReFirmLabs/binwalk";
+      flake = false;
+    };
+  };
+
+  outputs = {
+    self,
+    flake-utils,
+    naersk,
+    nixpkgs,
+    binwalk-git,
+  }:
+    flake-utils.lib.eachDefaultSystem (
+      system: let
+        pkgs = nixpkgs.legacyPackages.${system};
+        naersk' = pkgs.callPackage naersk {};
+        built = naersk'.buildPackage {
+          pname = "binwalk";
+          version = "3-beta";
+          src = "${binwalk-git}";
+          nativeBuildInputs = with pkgs; [pkg-config];
+          buildInputs = with pkgs; [fontconfig.dev xz.dev];
+        };
+      in {
+        packages.default = built;
+        apps = flake-utils.lib.mkApp {drv = built;};
+      }
+    );
+}
